a4bce3b7303c7c5b53e42b3784d18fffa65964be
Felipe <ah193971@gmail.com>
2026-01-15 22:13:41 -0600

    V0.1

--- main.go
+++ main.go
@@ -10,20 +10,44 @@ type Task struct {
 	gorm.Model
 	Name        string
 	Responsible string
-	State       bool
+	State       string
 	Priority    string
+}
+
+func create_task() Task {
+	fmt.Println("Enter the name, Responsible, state and Priority of the task: ")
+
+	var Name string
+	var Responsible string
+	var State string
+	var Priority string
+
+	n, err := fmt.Scanln(&Name, &Responsible, &State, &Priority)
+	if err != nil {
+		fmt.Println("something went wrong there: ", err)
+	}
+
+	newTask := Task{
+		Name:        Name,
+		Responsible: Responsible,
+		State:       State,
+		Priority:    Priority,
+	}
+
+	fmt.Println(n)
+	return newTask
 }
 
 func main() {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Sorry couldn't connect to the database: ", err)
+	}
 
-	if err != nil {
-		fmt.Println("Could not migrate the database:", err)
-		return
+	if err := db.AutoMigrate(&Task{}); err != nil {
+		fmt.Println("Migration failed: ", err)
 	}
-	err = db.AutoMigrate(&Task{})
-	if err != nil {
-		fmt.Println("Could not migrate the database: ", err)
-		return
-	}
+
+	task := create_task()
+	db.Create(&task)
 }
