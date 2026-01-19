96e34c8b36ab4245ec7338a4df18abf8b3372409
Felipe <ah193971@gmail.com>
2026-01-18 23:45:23 -0600

    V1.0

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -97,8 +98,96 @@ func listTasks(db *gorm.DB) {
 
 }
 
-// Delete the Scan function, doesn't do shit!
+func editTask(db *gorm.DB) {
+	var task Task
+
+	//Getting input from user
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Insert the id of the task to modify: ")
+	id_Str, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Couldn't read that string: ", err)
+		return
+	}
+	id_Str = strings.TrimSpace(id_Str)
+	ID, err := strconv.Atoi(id_Str)
+	if err != nil {
+		fmt.Println("Invalid ID", err)
+		return
+	}
+	if err := db.First(&task, ID).Error; err != nil {
+		fmt.Println("Task not found!")
+		return
+	}
+	fmt.Printf("Editing task: %s\n", task.Name)
+
+	//Changing name
+	fmt.Println("Enter the new task, or press enter to keep it!")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	if name != "" {
+		task.Name = name
+	}
+
+	// Changing Responsible
+	fmt.Println("Enter the name of the responsible for the task or leave it to keep it: ")
+	responsible, _ := reader.ReadString('\n')
+	responsible = strings.TrimSpace(responsible)
+	if responsible != "" {
+		task.Responsible = responsible
+	}
+
+	//Changing state
+	fmt.Println("Enter the state of the task, or leave it to keep it: ")
+	state, _ := reader.ReadString('\n')
+	state = strings.TrimSpace(state)
+	if state != "" {
+		task.State = state
+	}
+
+	//Changing priority
+	fmt.Println("Enter the priority, or leave it to keep it: ")
+	priority, _ := reader.ReadString('\n')
+	priority = strings.TrimSpace(priority)
+	if priority != "" {
+		task.Priority = priority
+	}
+
+	//Save the new task
+	db.Save(&task)
+	fmt.Println("Task save it successfully!")
+}
+
+func deleteTask(db *gorm.DB) {
+
+	var task Task
+
+	// Getting the ID from standard input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Insert the ID of the task to delete: ")
+	id_Str, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Some error just happened: ", err)
+		return
+	}
+	id_Str = strings.TrimSpace(id_Str)
+	ID, err := strconv.Atoi(id_Str)
+	if err != nil {
+		fmt.Println("Sorry couldn't parse that string: ", err)
+		return
+	}
+	if err := db.First(&task, ID).Error; err != nil {
+		fmt.Println("Task not found!")
+		return
+	}
+
+	db.Delete(&task)
+	fmt.Println("Task delete successfully!")
+}
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Sorry couldn't connect to the database: ", err)
@@ -107,14 +196,24 @@ func main() {
 		fmt.Println("Migration failed: ", err)
 	}
 
-	var menu int
-
 	for {
 		fmt.Println("Please what do you want to do?")
 		fmt.Println("1- To add a new task")
 		fmt.Println("2- To list all the tasks")
-		fmt.Println("3- To exit")
-		_, err := fmt.Scan(&menu)
+		fmt.Println("3- To edit the task")
+		fmt.Println("4- To delete the task")
+		fmt.Println("5- To exit")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Couldn't get the input")
+		}
+
+		input = strings.TrimSpace(input)
+
+		Input := strings.TrimSpace(input)
+		menu, err := strconv.ParseInt(Input, 10, 32)
+
 		if err != nil {
 			fmt.Println("Sorry wrong input: ", err)
 		}
@@ -130,6 +229,11 @@ func main() {
 		} else if menu == 2 {
 			listTasks(db)
 		} else if menu == 3 {
+			editTask(db)
+		} else if menu == 4 {
+			deleteTask(db)
+		} else if menu == 5 {
+			fmt.Println("Have a great day!")
 			break
 		}
 	}
